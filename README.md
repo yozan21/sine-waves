674e2e4f66424abb8c00598b1cccb49c1c740f47
yozan21 <yojankaphle80@gmail.com>
2024-05-01 11:05:07 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Circular Motion
+
+Welcome to this fun site. Mouse pointer is recommended for good visual effects.
+
+-Site link: [Live-site](https://yozan21.github.io/circular/)
---
+++ src/js/canvas.js
@@ -0,0 +1,72 @@
+import utils, { randomColor, randomIntFromRange } from "./utils";
+import * as dat from "dat.gui";
+
+const gui = new dat.GUI();
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+const wave = {
+  y: canvas.height / 2,
+  length: 0.01,
+  amplitude: 100,
+  frequency: 0.01,
+};
+const strokeColor = {
+  h: 0,
+  s: 50,
+  l: 50,
+};
+
+const bgColor = {
+  r: 0,
+  g: 0,
+  b: 0,
+  a: 0.01,
+};
+const waveFolder = gui.addFolder("wave");
+waveFolder.add(wave, "y", 0, canvas.height);
+waveFolder.add(wave, "length", -0.01, 0.01);
+waveFolder.add(wave, "amplitude", -300, 300);
+waveFolder.add(wave, "frequency", -0.01, 1);
+waveFolder.open();
+
+const strokeFolder = gui.addFolder("stroke");
+strokeFolder.add(strokeColor, "h", 0, 255);
+strokeFolder.add(strokeColor, "s", 0, 100);
+strokeFolder.add(strokeColor, "l", 0, 100);
+strokeFolder.open();
+
+const bgColorFolder = gui.addFolder("bgColor");
+bgColorFolder.add(bgColor, "r", 0, 255);
+bgColorFolder.add(bgColor, "g", 0, 255);
+bgColorFolder.add(bgColor, "b", 0, 255);
+bgColorFolder.add(bgColor, "a", 0, 1);
+bgColorFolder.open();
+
+let increment = wave.frequency;
+function animate() {
+  requestAnimationFrame(animate);
+  c.fillStyle = `rgba(${bgColor.r},${bgColor.g},${bgColor.b}, ${bgColor.a})`;
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  c.beginPath();
+  c.moveTo(0, canvas.height / 2);
+  for (let i = 0; i < canvas.width; i++) {
+    c.lineTo(
+      i,
+      wave.y +
+        Math.sin(i * wave.length + increment) *
+          wave.amplitude *
+          Math.sin(increment)
+    );
+  }
+  c.strokeStyle = `hsl(${Math.abs(strokeColor.h * Math.sin(increment))}, ${
+    strokeColor.s
+  }%, ${strokeColor.l}%)`;
+  c.stroke();
+  increment += wave.frequency;
+}
+
+animate();
